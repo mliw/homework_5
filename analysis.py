35d4a1eda03b92f66feaa56310ab9334d5db475d
mingweichina <mingweichina@126.com>
2020-07-15 10:17:56 +0800

    home

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 
 def parser_data(num=2019):
@@ -35,30 +36,54 @@ def cal_teams(home,away,data_cache):
     """
     Parameters
     ----------
-    home : STR
+    home : str
         The name of home team.
-    away : STR
+    away : str
         The name of away team.
     data_cache : tuple
         home_data,mean_home,mean_away.
     Returns
     -------
-    home_score and away_score
+    expected_home_score and expected_away_score
     """
     home_data,mean_home,mean_away = data_cache
-    
-    
+    expected_home_score = mean_home*home_data.loc[home,"attack_strength"]*home_data.loc[away,"defence_weakness"]
+    expected_away_score = mean_away*home_data.loc[away,"attack_strength"]*home_data.loc[home,"defence_weakness"]    
+    return expected_home_score,expected_away_score
     
 
+def cal_probability(expected_home_score,expected_away_score,tol=100):
+    md_home = stats.poisson(expected_home_score)
+    md_away = stats.poisson(expected_away_score)
+    p_draw = sum([md_home.pmf(i)*md_away.pmf(i) for i in range(tol)])
+    p_home_win = sum([(1-md_home.cdf(i))*md_away.pmf(i) for i in range(tol)])
+    p_home_loss = sum([(1-md_away.cdf(i))*md_home.pmf(i) for i in range(tol)])
+    return p_home_win,p_draw,p_home_loss
+    
+
+class assemble:
+    
+    def __init__(self,num=2019):
+        self.data_cache = parser_data(num)
+        home_data,mean_home,mean_away = data_cache
+        self.team_list = home_data.index
+        
+    def produce_result(self,home,away):
+        expected_home_score,expected_away_score = cal_teams(home,away,self.data_cache)
+        p_home_win,p_draw,p_home_loss = cal_probability(expected_home_score,expected_away_score)
+        print("Home:{}, Away:{},home_win:{},home_draw:{},home_loss:{}".format(home,away,np.round(p_home_win,4),np.round(p_draw,4),np.round(p_home_loss,4)))
+    
+    def produce_whole(self,):
+        print("="*70)
+        for i in range(len(self.team_list)):
+            for j in range(len(self.team_list)):        
+                if i!=j:
+                    self.produce_result(team_list[i],team_list[j])
+        print("="*70)
+        
+
 if __name__=="__main__":
-    
-    data_cache = parser_data(2019)
- Liverpool (home) and Tottenham
+    whole = assemble(2019)
+    whole.produce_whole()
  
-    home_data,mean_home,mean_away  
-    
-    
-home_str = "data/"+str(num)+"-"+str(num+1)+"-home.xlsx" 
-data = xlrd.open_workbook(home_str)
-table = data.sheet()[0]
  
